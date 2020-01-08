b2d8965ba2489ad90631d241e131395f148f9bb5
AKASH VERMA <akash.verma@qainfotech.com>
2020-01-08 10:57:09 +0530

    GetRequest structuring

--- src/main/java/sft/httpclient/GetRequest.java
+++ src/main/java/sft/httpclient/GetRequest.java
@@ -13,54 +13,26 @@ public class GetRequest {
 		authToken = SftSetup.generate_token();
 	}
 	
-	public String GetRequestWithOauth(final String endPoint) {
-		
-		Response response = hit_request(endPoint);
-		
+	public String getRequestWithOauth(final String endPoint) {
+		Response response = getRequest(endPoint);
 		if(response.getStatusCode() == 200)
 			return response.getBody().asString();
 		else
 			Assert.fail("[[[[[Error in api hit: " + endPoint + "]]]]]");
 		return null;
 	}
-
-	public JsonPath GetRequestWithOauthJSON(final String endPoint) {
-		
-		Response response = hit_request(endPoint);
-		if(response.getStatusCode() == 200)
-			return response.getBody().jsonPath();
-		else
-			Assert.fail("[[[[[Error in api hit: " + endPoint + "]]]]]");
-		return null;
-	}
 	
-	
-	
-	
-	private Response hit_request(final String endPoint)
+	private Response getRequest(final String endPoint)
 	{
-		 return RestAssured.given().header("Authorization", "Bearer " + authToken)
-				.contentType("application/json")
-				.get(endPoint);
+		 return RestAssured.given().header("Authorization", "Bearer " + authToken).contentType("application/json").get(endPoint);
 	}
 	
 	
-	private void throw_exception(final String endPoint)
-	{
-		try {
-			throw new Exception("[[[[[Error in api hit: " + endPoint + "]]]]]");
-		} catch (Exception e) {
-			e.printStackTrace();
-			System.exit(1);
-		}
-	}
-	
-	
+	//Testing
 	public static void main(String []args)
 	{
 		GetRequest get_request = new GetRequest();
-		System.out.println(get_request.GetRequestWithOauth("sobjects/Lead/describe/layouts"));
-		
+		System.out.println(get_request.getRequestWithOauth("sobjects/Lead/describe/layouts"));
 	}
 	
 }
