4acf0cdae1e3e5d1ea15de27236ef8d5239d3123
arun <arungirdar@qainfotech.com>
2020-01-23 12:47:56 +0530

    lead get and set

--- src/main/java/sft/SOQL/ExecuteLeadSample.java
+++ src/main/java/sft/SOQL/ExecuteLeadSample.java
@@ -1,40 +1,26 @@
 package sft.SOQL;
 
 import com.sforce.soap.enterprise.QueryResult;
+import com.sforce.soap.enterprise.sobject.Lead;
+
+import java.util.Date;
 
 public class ExecuteLeadSample {
     public static void main(String[] args) {
         QueryResult qr;
         ExecuteSOQL db = new ExecuteSOQL();
-        qr = db.ExecuteQuery("SELECT Id, FirstName, Status FROM Contact WHERE FirstName like '%' ORDER BY CreatedDate ASC NULLS FIRST");
-        System.out.println(qr.getRecords().length+"---");
-        boolean done = false;
-//        Lead[] lead = new Lead[2];
+        qr = db.ExecuteQuery("SELECT Id, FirstName, Status FROM Lead WHERE FirstName like 'TestNameUpdated%' ORDER BY CreatedDate ASC NULLS FIRST");
+        Lead lead = (Lead) qr.getRecords()[0];
+        //        Lead[] lead = new Lead[2];
+        System.out.println("FirstName : "+lead.getFirstName());
+        System.out.println("Status : "+lead.getStatus());
 
 
 
+        Date date = new Date();
 
-//        Lead[] lead = (Lead[]) qr.getRecords();
-//        System.out.println("FirstName : "+lead[0].getFirstName());
-//        System.out.println("Status : "+lead[0].getStatus());
-//        Date date = new Date();
-//
-//        lead[0].setFirstName("TestNameUpdated"+date.getTime());
-//
-//        db.UpdateObject(lead);
+        lead.setFirstName("TestNameUpdated"+date.getTime());
+
+        db.UpdateLeadObject(lead);
     }
-
-//    public void update(Sobject){
-//        Lead[] updates = new Lead[2];
-//
-//        Lead lead1 = new Lead();
-//        lead1.setId(ids[0]);
-//        account1.setShippingPostalCode("89044");
-//        updates[0] = account1;
-//
-//        Account account2 = new Account();
-//        account2.setId(ids[1]);
-//        account2.setNumberOfEmployees(1000);
-//        updates[1] = account2;
-//    }
 }
--- src/main/java/sft/SOQL/ExecuteSOQL.java
+++ src/main/java/sft/SOQL/ExecuteSOQL.java
@@ -1,7 +1,11 @@
 package sft.SOQL;
 
 import com.sforce.soap.enterprise.EnterpriseConnection;
+import com.sforce.soap.enterprise.Error;
 import com.sforce.soap.enterprise.QueryResult;
+import com.sforce.soap.enterprise.SaveResult;
+import com.sforce.soap.enterprise.sobject.Lead;
+import com.sforce.soap.enterprise.sobject.SObject;
 import com.sforce.ws.ConnectionException;
 
 public class ExecuteSOQL extends GetDatabaseConnection {
@@ -30,36 +34,33 @@ public class ExecuteSOQL extends GetDatabaseConnection {
         return qResult;
     }
 
-//    public boolean UpdateLeadObject(Lead[] updates) {
-////        Lead newlead = (Lead) dataObject;
-//        boolean flag = true;
-////        Lead[] updates = new Lead[2];
-//        connection = eDBConnection.login();
-////        System.out.println(newlead.getFirstName() + " : Being Updated");
-//        try {
-//            SaveResult[] results = connection.update(new SObject[]{updates});
-//            SaveResult[] saveResults = connection.update(updates);
-//            for (SaveResult saveResult : saveResults) {
-//                if (saveResult.isSuccess()) {
-//                    System.out.println("Successfully updated Lead ID: " + saveResult.getId());
-//                } else {
-//                    flag = false;
-//                    // Handle the errors.
-//                    // We just print the first error out for sample purposes.
-//                    Error[] errors = saveResult.getErrors();
-//                    if (errors.length > 0) {
-//                        System.out.println("Error: could not update " + "Lead ID "
-//                            + saveResult.getId() + ".");
-//                        System.out.println("\tThe error reported was: ("
-//                            + errors[0].getStatusCode() + ") "
-//                            + errors[0].getMessage() + ".");
-//                    }
-//                }
-//            }
-//
-//        } catch (ConnectionException e) {
-//            e.printStackTrace();
-//        }
-//        return flag;
-//    }
+    public boolean UpdateLeadObject(Lead lead) {
+        boolean flag = true;
+        try {
+            connection = eDBConnection.login();
+            System.out.println(lead.getFirstName() + " : Being Updated");
+            SaveResult[] results = connection.update(new SObject[]{lead});
+            for (SaveResult saveResult : results) {
+                if (saveResult.isSuccess()) {
+                    System.out.println("Successfully updated Account ID: " + saveResult.getId());
+                } else {
+                    flag = false;
+                    // Handle the errors.
+                    // We just print the first error out for sample purposes.
+                    Error[] errors = saveResult.getErrors();
+                    if (errors.length > 0) {
+                        System.out.println("Error: could not update " + "Account ID "
+                            + saveResult.getId() + ".");
+                        System.out.println("\tThe error reported was: ("
+                            + errors[0].getStatusCode() + ") "
+                            + errors[0].getMessage() + ".");
+                    }
+                }
+            }
+
+        } catch (ConnectionException e) {
+            e.printStackTrace();
+        }
+        return flag;
+    }
 }
