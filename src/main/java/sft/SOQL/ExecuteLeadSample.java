f89aa5065d5188f07ecd1b466e80699c203f2c76
arun <arungirdar@qainfotech.com>
2020-01-29 10:15:14 +0530

    lead to opp via DB

--- src/main/java/sft/SOQL/ExecuteLeadSample.java
+++ src/main/java/sft/SOQL/ExecuteLeadSample.java
@@ -9,13 +9,15 @@ public class ExecuteLeadSample {
     public static void main(String[] args) {
         QueryResult qr;
         ExecuteSOQL db = new ExecuteSOQL();
-        qr = db.ExecuteQuery("SELECT Id, FirstName, Status FROM Lead WHERE FirstName like 'TestNameUpdated%' ORDER BY CreatedDate ASC NULLS FIRST");
+        qr = db.ExecuteQuery("SELECT Id, FirstName, Status FROM Lead WHERE FirstName like 'auto1579854362680%' ORDER BY CreatedDate ASC NULLS FIRST");
         Lead lead = (Lead) qr.getRecords()[0];
         System.out.println("FirstName : "+lead.getFirstName());
         System.out.println("Status : "+lead.getStatus());
         Date date = new Date();
         lead.setFirstName("TestNameUpdated"+date.getTime());
         db.UpdateLeadObject(lead);
-//        db.convertLead();
+        Lead lto[] = new Lead[1];
+        lto[0] = lead;
+        db.convertLead(lto);
     }
 }
